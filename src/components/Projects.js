0a96aa01938a29e1d5c84089aacd4eb598807193
auchmargust <fengjingya320@gmail.com>
2024-01-09 23:00:29 -0800

    configure react app for deployment to Github

---
+++ src/components/Projects.js
@@ -0,0 +1,111 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects =() => {
+  const projectsList1 = [
+    {
+      title: "Machine Learning 4 Trading",
+      description: "Stock trading strategy formulated with machine learning using Python",
+      imgUrl: projImg1,
+    },
+  ]
+
+  const projectsList2 = [
+    {
+      title: "Natours",
+      description: "A Node.js RESTful web application",
+      imgUrl: projImg2,
+    },
+  ]
+
+  const projectsList3 = [
+    {
+      title: "Digital Temptations",
+      description: "Digital Temptations",
+      imgUrl: projImg3,
+    },
+  ]
+
+
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__slideInUp": ""}>
+                <h2>Projects</h2>
+                <p> Explore my projects showcasing the fusion of my technical prowess with a touch of ingenuity... </p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Machine Learning</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Web Development</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">HCI</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projectsList1.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                        {
+                          projectsList2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    <Row>
+                        {
+                          projectsList3.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      {/* <img className="background-image-right" src={colorSharp2}></img> */}
+    </section>);
+}
